760f85d7d8ff0b5880b8c9fe5e4d1d130a32bc12
victorAlvarez <victor87eg6@gmail.com>
2014-08-03 01:33:00 +0200

    Integración de layouts

--- app/routes.php
+++ app/routes.php
@@ -11,13 +11,26 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
-
 // route to show the login form
 Route::get('login', array('uses' => 'HomeController@showLogin'));
 
 // route to process the form
 Route::post('login', array('uses' => 'HomeController@doLogin'));
+
+Route::get('/', function()
+{
+    return View::make('pages.home');
+});
+Route::get('about', function()
+{
+    return View::make('pages.about');
+});
+Route::get('projects', function()
+{
+    return View::make('pages.projects');
+});
+Route::get('contact', function()
+{
+    return View::make('pages.contact');
+});
+
